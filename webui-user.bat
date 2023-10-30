f8b8202d6779a7365e0fde4e070b242759f9327f
kllioo3666 <149438617+kllioo3666@users.noreply.github.com>
2023-10-31 07:07:02 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\win11\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.10
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
-
+git pull
 call webui.bat
